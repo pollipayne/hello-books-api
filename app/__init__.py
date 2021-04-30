5ff182ec30d85cd9bc6e9b7d40388904a1240995
Brittney Payne <ohoptional@gmail.com>
2021-04-29 19:59:44 -0700

    Changed table name

--- app/__init__.py
+++ app/__init__.py
@@ -12,13 +12,14 @@ migrate = Migrate()
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    # from .routes import hello_world_bp
-    # app.register_blueprint(hello_world_bp)
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/ada_books'
 
     db.init_app(app)
     migrate.init_app(app, db)
+    from .routes import hello_world_bp
+    app.register_blueprint(hello_world_bp)
 
     from app.models.book import Book
 
--- app/models/book.py
+++ app/models/book.py
@@ -1,7 +1,11 @@
 from app import db 
 
-class Book(db.Model):
-    __tablename__ = "books_table"
+class Book(db.Model): #an instance of SQLAlchemy Books is inheriting 
+    __tablename__ = "books"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    #creates a DB column called ID, of type INT, assigns it as PK & tells it 
+    #to auto increment 
     title = db.Column(db.String)
+    # creates a DB column called title, sets it of type String
     description = db.Column(db.String)
+    # creates a DB column called description, sets it of type String
