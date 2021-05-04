ea88471d9abe5f3d0e5bb0bd47a35442666cf461
Brittney Payne <ohoptional@gmail.com>
2021-05-03 18:20:23 -0700

    Added testing for single book route and fixture for testing db

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest 
 from app import create_app 
 from app import db 
+from app.models.book import Book 
+
 
 
 @pytest.fixture
@@ -19,3 +21,14 @@ def app():
 def client(app):
     return app.test_client()
 
+@pytest.fixture
+def two_saved_books(app):
+    # here we are setting up an arrange step for 
+    #that we can reuse for several tests 
+    noodle_book = Book(title="Noodles!", 
+                        description="All noodles all the time.")
+    bean_book = Book(title="Beans!", 
+                    description="All beans all the time!")
+
+    db.session.add_all([noodle_book, bean_book])
+    db.session.commit()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -18,10 +18,20 @@ def test_get_all_books_with_no_records(client):
 
 
 #get one book by id 
-def test_get_one_book_by_id(client):
+def test_get_one_book_by_id(client, two_saved_books):
+    #Act
     response = client.get("/books/1")
     response_body = response.get_json()
+
+    #assert 
+    assert response.status_code == 200
+    assert response_body == { "id": 1, 
+                            "title": "Noodles!", 
+                            "description": "All noodles all the time."}
     
+
+
+
 
 # update one book by id 
 
